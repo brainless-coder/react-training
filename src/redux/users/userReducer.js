cd5444511ebaef5821ca58be1bd1cff664b05762
brainless-coder <singhamish27@gmail.com>
2022-02-10 00:19:24 +0530

    added redux

--- src/redux/users/userReducer.js
+++ src/redux/users/userReducer.js
@@ -1,5 +1,39 @@
 import { userDataConstants } from './actionTypes';
 
 const initialState = {
-  
+  loader: false,
+  userData: [],
+  error: ""
 }
+
+const userReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case userDataConstants.USERS_DATA_INITIATE: {
+      return {
+        ...state,
+        loader: true,
+        userData: [],
+        error: ""
+      }
+    }
+    case userDataConstants.USERS_DATA_SUCCESS: {
+      return {
+        ...state,
+        loader: false,
+        userData: payload
+      }
+    }
+    case userDataConstants.USERS_DATA_FAILURE: {
+      return {
+        ...state,
+        loader: false,
+        error: payload
+      }
+    }
+    default:  return state;
+  }
+};
+
+
+export default userReducer;
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,28 +1,33 @@
-import {useEffect, useState} from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Container } from "reactstrap";
 import ProjectCard from "../components/ProjectCard";
+import { userDataInitiate, userDataSuccess } from '../redux/users/action';
 
 export default function Projects() {
-  const [users, setUsers] = useState([]);
+  // const [users, setUsers] = useState([]);
+  const dispatch = useDispatch();
+  const users = useSelector(state => state.user);
 
   const getUsers = async () => {
     const res = await fetch('https://reqres.in/api/users');
     const data = await res.json();
     const usersData = data.data;
-    setUsers(usersData);
+    dispatch(userDataSuccess(usersData));
   }
 
+
   useEffect(() => {
+    dispatch(userDataInitiate());
     getUsers();
   }, []);
-
 
 
   return (
     <Container>
       <h1>Projects Page</h1>
       {
-      users.map((user) => (
+      users.userData.map((user) => (
         <ProjectCard
           imgSrcUrl={user.avatar}
           lastName={user.last_name}
